3f7d51d41d78e4f105e1264df7a78e0b0c09edfa
Philip Olafsen <ph.olafsen@gmail.com>
2023-06-02 13:40:23 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Uke_5
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
